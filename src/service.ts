311822553a9bec7e429651aa1dbd1b9e2bcb80bd
Han Che <han.che@selfbits.org>
2016-09-16 00:22:26 +0200

    test 2

---
+++ src/service.ts
@@ -0,0 +1,11 @@
+
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class SampleService {
+	public title: string = "Sample service";
+
+	constructor() {
+		console.log('hello from sample service');
+	}
+}
