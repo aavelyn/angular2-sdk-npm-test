f4a742f7346c8e8b27acdb8330f97f25823c910c
Han Che <han.che@selfbits.org>
2016-09-20 16:24:18 +0200

    added test and opaque token

--- src/angularselfbits.ts
+++ src/angularselfbits.ts
@@ -1,18 +1,17 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import {NgModule, ModuleWithProviders} from '@angular/core';
 import { Injectable } from '@angular/core';
 
-import {
-	SelfbitsAuth,
-	SelfbitsDatabase,
-	SelfbitsFile,
-	SelfbitsUser,
-	SelfbitsDevice,
-	SelfbitsPush
-} from './services/';
 
 import { SelfbitsAppConfig } from './utils/interfaces';
 
 import { HttpModule } from '@angular/http';
+import {SELFBITS_CONFIG} from "./utils/tokens";
+import {SelfbitsAuth} from "./services/auth";
+import {SelfbitsDatabase} from "./services/database";
+import {SelfbitsFile} from "./services/file";
+import {SelfbitsUser} from "./services/user";
+import {SelfbitsDevice} from "./services/device";
+import {SelfbitsPush} from "./services/push";
 
 @Injectable()
 export class SelfbitsAngular {
@@ -39,9 +38,11 @@ export const SELFBITS_PROVIDERS:any[] = [
 
 export const SelfbitsSetup = (appConfig: SelfbitsAppConfig): any => {
 		return [
-			{ provide: 'SelfbitsConfig', useValue: appConfig }
+			{ provide: SELFBITS_CONFIG, useValue: appConfig }
 		]
 };
+
+
 
 // RC5+ using ngModule to load providers
 @NgModule({
@@ -54,7 +55,7 @@ export class SelfbitsAngularModule{
 		return {
 			ngModule:SelfbitsAngularModule,
 			providers:[
-				{ provide: 'SelfbitsConfig', useValue: config }
+				{ provide: SELFBITS_CONFIG, useValue: config }
 			]
 		}
 	}
